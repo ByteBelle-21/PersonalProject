5abcfa4a3ace5e1b4666b63b9c103a7adb125ced
ByteBelle-21 <jrg814@usask.ca>
2024-12-27 13:13:49 -0500

    Prepared new skeleton for peoject

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 5000;
+const multer = require('multer');
+const PORT = 4000;
 
 const app = express();
 app.use(cors());
@@ -9,27 +10,28 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 const mysql = require('mysql2');
+const upload = multer({ storage: multer.memoryStorage() });
+
 
 var database = mysql.createConnection({
-  host: 'mysql',
+  host: 'mysql-image',
   user: 'root',
-  password: 'abcd1234'
+  password: 'abcdefgh'
  
 });
 
 
 database.connect((error) => {
-    if (error) {
-        console.error('Error connecting to the database:', error);
-        if (error.code === 'ECONNREFUSED') {
-            console.error('Database connection refused. Exiting application.');
-            process.exit(1); 
-        }
-    } else {
-        console.log('Connected to the database.');
-        //createDatabase();
-    }
-  });
+  if (error) {
+      console.error('Error connecting to the database:', error);
+      if (error.code === 'ECONNREFUSED') {
+          console.error('Database connection refused. Exiting application.');
+          process.exit(1); 
+      }
+  } else {
+      console.log('Connected to the database.');
+  }
+});
 
 
 app.listen(PORT, () => {
