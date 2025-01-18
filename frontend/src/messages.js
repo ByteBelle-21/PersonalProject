159527564dfc9c30cd6cd421e8d19bebb60c336c
prajaktaS <jrg814@usask.ca>
2025-01-18 16:28:20 -0600

    Finished layout for direct message page

--- frontend/src/messages.js
+++ frontend/src/messages.js
@@ -9,13 +9,15 @@ import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
+import TextareaAutosize from 'react-textarea-autosize';
 
 function Messages(){
     return(
         <div className="channels">
            <div className='message-left-block'>
                 <p className='fw-bold' style={{margin:'1vh'}}>Other Direct Messages</p>
-                <ListGroup as="ol">
+                <hr></hr>
+                <ListGroup as="ol" className='direct-messages'>
                     <ListGroup.Item
                         as="li"
                         className="d-flex justify-content-between align-items-start direct-message-item">
@@ -88,15 +90,34 @@ function Messages(){
                         </div>
                         <div className="ms-2 me-auto">
                         <div className="fw-bold">sdbv sdcjsdc </div>
-                            <Link className='view-link'>View conversations</Link>
+                            <Link className='view-link'>ehfkh fb wh</Link>
                         </div>
                     </ListGroup.Item> 
                 </ListGroup>
 
            </div>
-           <div className='middle-block'>
+           <div className='message-middle-block'>
                <div className='connected-user-logo-block'>
-
+                    <Image  src="Group 301.png"  className="connected-user-img"  roundedCircle />
+                    <div className='connected-user-name'>
+                        <span className='fw-bold'>jsdh sdh sjh hb</span>
+                        <span style={{fontSize:'small', opacity:'70%', fontWeight:'bold'}}>ByteBelle</span>
+                    </div>
+               </div>
+               <div className='me-auto message-block'>
+                    
+               </div>
+               <div className='textarea-block'>
+                    <Link style={{color:'black', marginRight:'1vh', opacity:'70%'}}>
+                        <span class="material-symbols-outlined">attach_file</span>
+                    </Link> 
+                    <TextareaAutosize placeholder="Add your message here"  className='text-area-formcontrol' />
+                    <Link style={{color:'black', marginRight:'1vh', opacity:'70%'}}>
+                        <span class="material-symbols-outlined">add_reaction</span>
+                    </Link> 
+                    <Link style={{color:'black', marginLeft:'1vh'}}>
+                        <span class="material-symbols-outlined">send</span>
+                    </Link> 
                </div>
            </div>
            <div className='message-right-block '>
@@ -107,8 +128,19 @@ function Messages(){
                         <p style={{fontSize:'medium'}}>ByteBelle</p>
                         <p >Nitys is jhfjcherf hf fh f fejhfjhc sbsc jshd cbc sjd c hvsjd vbjdvbj vs vjh </p>
                     </ListGroup.Item>
-                    <ListGroup.Item as="li"  className='profile-item'>
-            
+                    <ListGroup.Item as="li"  className='social-media-item'>
+                        <Stack direction='horizontal' gap={4}>
+                            <Link >
+                                <Image  src="facebook.png"  className="social-media-img"  roundedCircle />
+                            </Link>
+                            <Link>
+                                <Image  src="instagram.png"  className="social-media-img"  roundedCircle />
+                            </Link>
+                            <Link>
+                                <Image  src="linkedin.png"  className="social-media-img"  roundedCircle />
+                            </Link>
+                        </Stack>
+                     
                     </ListGroup.Item>
                     <ListGroup.Item className='profile-skills-item' as="li">
                         <hr></hr>
@@ -132,23 +164,15 @@ function Messages(){
                         <p  style={{fontWeight:'bold' }}>Browse Tanya's posts</p>
                         <div className='activity-block'>
                             <ListGroup as="ol" className='activity-list'>
-                                <ListGroup.Item as="li">
+                                <ListGroup.Item as="li" className='activity-list-item'>
                                     <div className="fw-bold" style={{color:'#d84434'}}>sdbv sdcjsdc </div>
                                     <p style={{fontSize:'small'}} >erjhbgejhf ehrf jvef efhfefkve ergkerj ef erfer .....</p>
                                 </ListGroup.Item>
-                                <ListGroup.Item as="li">
+                                <ListGroup.Item as="li" className='activity-list-item'>
                                     <div className="fw-bold" style={{color:'#d84434'}}>sdbv sdcjsdc </div>
                                     <p style={{fontSize:'small'}} >erjhbgejhf ehrf jvef efhfefkve ergkerj ef erfer .....</p>
                                 </ListGroup.Item>
-                                <ListGroup.Item as="li">
-                                    <div className="fw-bold" style={{color:'#d84434'}}>sdbv sdcjsdc </div>
-                                    <p style={{fontSize:'small'}} >erjhbgejhf ehrf jvef efhfefkve ergkerj ef erfer .....</p>
-                                </ListGroup.Item>
-                                <ListGroup.Item as="li">
-                                    <div className="fw-bold" style={{color:'#d84434'}}>sdbv sdcjsdc </div>
-                                    <p style={{fontSize:'small'}} >erjhbgejhf ehrf jvef efhfefkve ergkerj ef erfer .....</p>
-                                </ListGroup.Item>
-                                <ListGroup.Item as="li">
+                                <ListGroup.Item as="li" className='activity-list-item'>
                                     <div className="fw-bold" style={{color:'#d84434'}}>sdbv sdcjsdc </div>
                                     <p style={{fontSize:'small'}} >erjhbgejhf ehrf jvef efhfefkve ergkerj ef erfer .....</p>
                                 </ListGroup.Item>
